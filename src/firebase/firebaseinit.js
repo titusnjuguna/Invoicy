9b29632f45960cb5c8fd2ab3b24cc200df9c0243
titus njuguna <titusnjuguna59@gmail.com>
2022-06-02 08:28:09 +0300

    add test

---
+++ src/firebase/firebaseinit.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDQyaXMBEl5ijR3Nwd-NtGYX2JIPe4zxC4",
+    authDomain: "invoicey-48ce9.firebaseapp.com",
+    projectId: "invoicey-48ce9",
+    storageBucket: "invoicey-48ce9.appspot.com",
+    messagingSenderId: "107556795919",
+    appId: "1:107556795919:web:e6b7794c16c8b45b6d957a"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp.firestore();
+
